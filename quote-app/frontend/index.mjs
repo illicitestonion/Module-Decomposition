26f9d2a2b6109163859c9296c1d822369a117bad
Daniel Wagner-Hall <dawagner@gmail.com>
2025-03-27 10:04:04 +0000

    Add quote app

---
+++ quote-app/frontend/index.mjs
@@ -0,0 +1,63 @@
+const server = "http://127.0.0.1:3000/";
+
+const fetchQuote = async () => {
+    const rawResponse = await fetch(server);
+    if (!rawResponse.ok) {
+        displayFeedback("Error fetching quote", rawResponse);
+        return;
+    }
+    try {
+    const response = await rawResponse.json();
+        const {author, quote} = response;
+        const quoteElement = document.querySelector("#quote-template").content.cloneNode(true);
+        quoteElement.querySelector(".quote").innerText = quote;
+        quoteElement.querySelector(".author").innerText = author;
+        const container = document.querySelector("#quote-container");
+        container.innerText = "";
+        container.appendChild(quoteElement);
+    } catch (e) {
+        displayFeedback("Error fetching quote", e);
+    }
+};
+
+const saveQuote = async (event) => {
+    event.preventDefault();
+    const quote = takeValue("#quote");
+    const author = takeValue("#author");
+
+    const rawResponse = await fetch(server, {
+        "method": "POST",
+        "body": JSON.stringify({
+            quote,
+            author,
+        }),
+    });
+    if (!rawResponse.ok) {
+        displayFeedback("Error saving quote", rawResponse);
+        return;
+    }
+    displayFeedback("Quote saved");
+};
+
+const takeValue = (selector) => {
+    const element = document.querySelector(selector);
+    const value = element.value;
+    element.value = "";
+    return value;
+};
+
+const displayFeedback = (message, error) => {
+    const feedbackElement = document.querySelector("#feedback");
+    if (error) {
+        console.error(error);
+        feedbackElement.classList.add("error");
+    } else {
+        feedbackElement.classList.remove("error");
+    }
+    feedbackElement.innerText = message;
+}
+
+document.querySelector("#fetch-quote").addEventListener("click", fetchQuote);
+document.querySelector("#save-quote").addEventListener("click", saveQuote);
+
+fetchQuote();
