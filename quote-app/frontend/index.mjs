bd2368086876805cceba29015320c461809c5bcb
Daniel Wagner-Hall <dawagner@gmail.com>
2025-03-27 10:18:36 +0000

    Update to deployed backend URL

--- quote-app/frontend/index.mjs
+++ quote-app/frontend/index.mjs
@@ -1,4 +1,4 @@
-const server = "http://127.0.0.1:3000/";
+const server = "https://dwh-quote-backend.hosting.codeyourfuture.io/";
 
 const fetchQuote = async () => {
     const rawResponse = await fetch(server);
